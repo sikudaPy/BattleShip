d78541b592aeef98d1b7dca66ea9a67be4cd1de4
ya.sikuda <ya.sikuda@yandex.ru>
2026-09-25 19:07:50 +0300

    add /data

--- BattleShiptGame/BattleShiptGame/settings.py
+++ BattleShiptGame/BattleShiptGame/settings.py
@@ -81,7 +81,7 @@ print(BASE_DIR / 'data' / 'db.sqlite3')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
     }
 }
 
